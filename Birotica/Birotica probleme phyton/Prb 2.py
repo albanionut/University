6ebba3d513f1b38354bb177d3288fe59cb083c09
albanionut <39435030+albanionut@users.noreply.github.com>
2019-10-10 18:55:27 +0300

    Add files via upload

---
+++ Birotica/Birotica probleme phyton/Prb 2.py
@@ -0,0 +1,5 @@
+sir1="Ion traiste "
+sir2="in panciu"
+sir3=sir1+sir2
+print(sir3)
+        
---
+++ Birotica/Birotica probleme phyton/Problema 1).py
@@ -0,0 +1,8 @@
+n=129
+s=0
+aux=n
+while (n>0):
+    c=n%10
+    s=s+c
+    n=n//10
+print("Suma cifrelor numarului ",aux," este ",s)
